07497641ae0d54fa52ed76b45716f19933eda7bf
GITSangWoo <dogcenta@gmail.com>
2024-08-27 12:50:12 +0900

    add chat

--- src/sb_works/TM_chat.py
+++ src/sb_works/TM_chat.py
@@ -4,22 +4,27 @@ from datetime import datetime
 import time
 import threading
 import json
-
+import sys
 import os
 
+username=input("사용자 입력:")
+
+def clear_screen():
+    sys.stdout.write("\033[H\033[J")
+
+messages = []
+
 def prochat():
+    global username 
     p = KafkaProducer(
         bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
         #bootstrap_servers=["localhost:29092"],
         value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
-    username = input("사용자 이름:")
-    print("채팅 프로그램 - 메시지 발신자")
-    print("메시지를 입력하세여.(종료시 'exit' 입력")
 
     while True:
-        msg = input(f"{username}: ")
+        msg = input()
         if msg.lower() == 'exit':
             break
         if msg == "@이름변경":
@@ -31,30 +36,32 @@ def prochat():
         p.flush()
 
 def conchat():
+    global username
     consumer = KafkaConsumer(
           'chat3',
           bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
           #bootstrap_servers=["localhost:29092"],
-          auto_offset_reset='earliest',
+          auto_offset_reset='latest',
           # auto_offset_reset='latest',
           enable_auto_commit = True,
-          group_id = 'chat-group',
+          group_id = 'chat-group4',
           value_deserializer=lambda x: loads(x.decode('utf-8'))
 )
 
 
-    print("채팅 프로그램 - 메시지 수신")
-    print("메시지 대기 중 ....")
-
     try:
         for m in consumer:
             data= m.value
-            
             timeparse=datetime.fromtimestamp(data['time'])
+            messages.append(f"[{data['user']}]:[{str(timeparse)}] {data['message']}")
+            clear_screen()
             ## data["time"]=str(timeparse)  ####
+            for message in messages: 
+                print(message)
 
-            print(f"[{data['user']}]:[{str(timeparse)}] {data['message']}")
-    
+            sys.stdout.write(f"{username}:")
+            sys.stdout.flush()    
+                
             ###################################
             timeparse=datetime.fromtimestamp(data['time'])
             curDir=os.getcwd()
@@ -66,21 +73,23 @@ def conchat():
                 f.write("\n")
             ###################################
 
-
     except KeyboardInterrupt:
         print("채팅 종료")
     finally:
         consumer.close()
 
+#if __name__ == "__main__":
+#    print("채팅 프로그램 - 메시지 발신 및 수신")
+#    username = input("사용할 이름을 입력하세요 : ")
 
-# 스레드 생성
+    # 스레드 생성
 producer_thread = threading.Thread(target=prochat)
 consumer_thread = threading.Thread(target=conchat)
 
-# 스레드 시작
+    # 스레드 시작
 producer_thread.start()
 consumer_thread.start()
 
-# 스레드 종료 대기
+    # 스레드 종료 대기
 producer_thread.join()
 consumer_thread.join()
