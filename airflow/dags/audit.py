16bed2ddf065e015365c15ce9ba35a6238529280
Mingk42 <xoals123456t@gmail.com>
2024-08-27 16:46:32 +0900

    audit ok

--- airflow/dags/audit.py
+++ airflow/dags/audit.py
@@ -2,24 +2,20 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from textwrap import dedent
 
-# Operators; we need this to operate!
 
 from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import (
-    ExternalPythonOperator,
     PythonOperator,
-    PythonVirtualenvOperator,
-    is_venv_installed,
     PythonVirtualenvOperator,
     BranchPythonOperator,
 )
 
+import os
+
+
 with DAG(
     'multichat',
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
     default_args={
         'depends_on_past': False,
         'retries': 1,
@@ -27,7 +23,7 @@ with DAG(
     },
     description='multi chat DAG',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(2015, 1, 1),
+    start_date=datetime(2024, 8, 25),
     #end_date=datetime(2015, 1, 5),
     catchup=True,
     tags=['spark'],
@@ -39,35 +35,111 @@ with DAG(
     start = EmptyOperator(task_id='start')
     end = EmptyOperator(task_id='end', trigger_rule="all_done")
 
+    def chk():
+        curDir=os.getcwd()
+        if os.path.exists(f"{curDir}/../../data"):
+            return "rm.dir"
+        else:
+            return "save"
 
-    check = BashOperator(
-            task_id="check",
-            bash_command="bash {{ var.value.CHECK_SH }} {{ds_nodash}}"
-#            echo "check"
-#            DONE_PATH=~/data/done/{{ds_nodash}}
-#            DONE_PATH_FILE="${DONE_PATH}/_DONE"
-#
-#            #파일 존재 여부 확인
-#            if [ -e "$DONE_PATH_FILE" ]; then
-#                figlet "Let's move on"
-#                exit 0
-#            else
-#                echo "I'll be back => $DONE_PATH_FILE"
-#                exit 1
-#            fi
+    def chkLog(ds_nodash):
+        homePath=os.path.expanduser("~")
+        print(f"{homePath}/code/SB_Works/logs/chat_{ds_nodash}.log")
+
+        if os.path.isfile(f"{homePath}/code/SB_Works/logs/chat_{ds_nodash}.log"):
+            return "check.parquet"
+        
+        return "send.rst"
+
+    def sv(ds_nodash):
+        import re
+        import pandas as pd
+
+        homePath=os.path.expanduser("~")
+
+        with open(f"{homePath}/code/SB_Works/logs/chat_{ds_nodash}.log") as f:
+            log_data=f.read()
+
+        json_pattern = re.compile(r'{.*?}')
+        json_matches = json_pattern.findall(log_data)
+        _json=[]
+        for j in json_matches:
+            _json.append(eval(j))
+        
+        df=pd.DataFrame(_json)
+
+        pattern = re.compile('[\w\s\!\@\#\$\%\&\(\)\~\*\-\=\+\_]')
+
+        df["message"]=df["message"].apply(lambda x:"".join(pattern.findall(x)))
+        df["user"]=df["user"].apply(lambda x:"".join(pattern.findall(x)))
+
+        os.makedirs(f"{homePath}/code/SB_Works/parquet/", exist_ok=True)
+        df.to_parquet(f"{homePath}/code/SB_Works/parquet/chat_{ds_nodash}.parquet")
+
+
+    def send_rst():
+        from kafka import KafkaProducer
+        import time
+        import threading
+        import json
+
+        def prochat():
+            p = KafkaProducer(
+                bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
+                #bootstrap_servers=["localhost:29092"],
+                value_serializer=lambda x: json.dumps(x).encode('utf-8')
+        )
+
+
+            while True:
+                data = {'message' : "task all success", 'time':time.time(), 'user': "AIRFLOW"}
+
+                p.send('chat3',value=data)
+                p.flush()
+
+                raise KeyboardInterrupt
+        # 스레드 생성
+        producer_thread = threading.Thread(target=prochat)
+
+        # 스레드 시작
+        producer_thread.start()
+
+        # 스레드 종료 대기
+        producer_thread.join()
+
+
+    check = BranchPythonOperator(
+            task_id="check.parquet",
+            python_callable=chk
     )
 
     rm_dir = BashOperator(
             task_id='rm.dir',
-            bash_command='rm -rf ~/tmp/test_parquet/load_dt={{ ds_nodash }}',
+            bash_command="""
+                rm ~/code/SB_Works/parquet/chat_{{ds_nodash}}.parquet
+            """,
+            )
+    
+    chkLog = BranchPythonOperator(
+            task_id="chk.log.exist",
+            python_callable=chkLog,
             )
 
-    save = BashOperator(
-            task_id='save',
-            bash_command="""
-            """
-    )
+    save = PythonOperator(
+            task_id="save",
+            python_callable=sv,
+            trigger_rule="one_success"
+            )
 
+    send_rst=PythonVirtualenvOperator(
+            task_id="send.rst",
+            python_callable=send_rst,
+            requirements=["kafka-python"],
+            trigger_rule="none_failed"
+            )
 ###################################################################
 
-    start >> [check, rm_dir] >> save >> end
+    start >> chkLog
+    chkLog >> check >> rm_dir >> save >> send_rst  >> end
+    chkLog >> send_rst
+    check >> save
